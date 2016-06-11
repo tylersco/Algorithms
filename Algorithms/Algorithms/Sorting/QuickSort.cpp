ef1232b06a8bfd0dbf737138006b22ab4a45aae9
tylersco <tysc7237@colorado.edu>
2016-06-11 12:59:33 -0600

    Add randomized quicksort

--- Algorithms/Algorithms/Sorting/QuickSort.cpp
+++ Algorithms/Algorithms/Sorting/QuickSort.cpp
@@ -11,6 +11,8 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 int partition(int *arr, int p, int r) {
     
@@ -37,12 +39,35 @@ int partition(int *arr, int p, int r) {
     
 }
 
+int randomPartition(int *arr, int p, int r) {
+    //Initialize random seed
+    srand(time(NULL));
+    
+    int random_index = rand() % (r - p) + p;
+    int temp = arr[r];
+    arr[r] = arr[random_index];
+    arr[random_index] = temp;
+    
+    int q = partition(arr, p, r);
+    
+    return q;
+}
+
 void QuickSort(int *arr, int p, int r) {
     //Use divide and conquer paradigm
     if(p < r) {
         int q = partition(arr, p, r);
         QuickSort(arr, p, q - 1);
         QuickSort(arr, q + 1, r);
+    }
+}
+
+void RandomizedQuickSort(int *arr, int p, int r) {
+    //Use divide and conquer paradigm
+    if(p < r) {
+        int q = randomPartition(arr, p, r);
+        RandomizedQuickSort(arr, p, q - 1);
+        RandomizedQuickSort(arr, q + 1, r);
     }
 }
 
@@ -55,6 +80,15 @@ int main(int argc, const char * argv[]) {
     
     for(int i = 0; i < n; i++)
         std::cout << arr[i] << " ";
+    std::cout << "\n";
+    
+    int arr2[] = {11, 7, 9, -3, 5, 2, 7, 13, -4};
+    int n2 = 9;
+    
+    RandomizedQuickSort(arr2, 0, n2 - 1);
+    
+    for(int i = 0; i < n2; i++)
+        std::cout << arr2[i] << " ";
     std::cout << "\n";
     
 }
